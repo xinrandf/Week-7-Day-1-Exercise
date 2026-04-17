83f77dc8fa1b60474b0d83978c71ef0db67bf312
xinrandf <xinrandee@gmail.com>
2026-04-16 20:38:26 -0700

    wip2

---
+++ src/ReactVertexSphere/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Scene from "./Scene";
+
+function Main() {
+  return (
+    <div>
+      <h2>ReactVertexSphere</h2>
+      <Scene />
+    </div>
+  );
+}
+
+export default Main;
