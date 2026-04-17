aa36f1b3a194126e21fee4a6879123d0cf6f9061
xinrandf <xinrandee@gmail.com>
2026-04-17 16:29:02 -0700

    Week 7 Day 1 Exercise
    Week 7 Day 1 Exercise

--- src/CollapsibleTree/Main.js
+++ src/CollapsibleTree/Main.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Tree from "./Tree";
+import data from "./data";
 
 function Main() {
+  const [orientation, setOrientation] = useState("vertical");
+  const [linkType, setLinkType] = useState("step");
+
   return (
     <div>
-      <h2>Collapsible Tree</h2>
-      <Tree />
+      <div style={{ marginBottom: "12px" }}>
+        <label>
+          orientation:
+          <select
+            value={orientation}
+            onChange={(e) => setOrientation(e.target.value)}
+            style={{ marginLeft: "6px", marginRight: "16px" }}
+          >
+            <option value="vertical">vertical</option>
+            <option value="horizontal">horizontal</option>
+          </select>
+        </label>
+
+        <label>
+          link:
+          <select
+            value={linkType}
+            onChange={(e) => setLinkType(e.target.value)}
+            style={{ marginLeft: "6px" }}
+          >
+            <option value="step">step</option>
+            <option value="curve">curve</option>
+            <option value="line">line</option>
+          </select>
+        </label>
+      </div>
+
+      <Tree
+        data={data}
+        orientation={orientation}
+        linkType={linkType}
+        layout="cartesian"
+        stepPercent={0.5}
+      />
     </div>
   );
 }
--- src/ReactVertexSphere/Main.js
+++ src/ReactVertexSphere/Main.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Canvas } from "@react-vertex/core";
 import Scene from "./Scene";
 
 function Main() {
   return (
     <div>
-      <h2>ReactVertexSphere</h2>
-      <Scene />
+      <h2>React Vertex Sphere of Spheres</h2>
+
+      <div style={{ width: "100%", height: "600px" }}>
+        <Canvas
+          style={{ width: "100%", height: "100%" }}
+          renderOnUpdate={true}
+        >
+          <Scene />
+        </Canvas>
+      </div>
     </div>
   );
 }
